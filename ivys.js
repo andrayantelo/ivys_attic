f75e8dcd89bc327c94a931c6420076a7d1182469
andrayantelo <andrayantelo@gmail.com>
2017-02-27 16:21:38 -0800

    added isotope plugin, changed images to thumbnails

--- ivys.js
+++ ivys.js
@@ -1,8 +1,5 @@
-var $grid = $('.grid').imagesLoaded( function() {
-  // init Masonry after all images have loaded
-    $('.grid').masonry({
-        // options
-        itemSelector: '.grid-item',
-        columnWidth: 200
-    });
+$('.grid').isotope({
+  // options
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
 });
