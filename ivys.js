80742b0274eecb3398b81ca115fe6a2f4450abd6
andrayantelo <andrayantelo@gmail.com>
2017-03-03 13:01:49 -0800

    trying to figure out isotope

--- ivys.js
+++ ivys.js
@@ -1,5 +1,16 @@
-$('.grid').isotope({
-  // options
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows'
-});
+
+ -$('.grid').isotope({		 +$(document).ready(function() {
+	
+    $grid = $('.grid').isotope({
+        // options
+        itemSelector: '.grid-item',
+        layoutMode: 'masonry',
+        percentPosition: true,
+        masonry: {
+            // use element for option
+            columnWidth: 100
+        }
+        });
+    });
+
+});		  });
