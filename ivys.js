0e532be0c0169dac420a38d910d01514f4fd049c
andrayantelo <andrayantelo@gmail.com>
2017-03-07 14:29:09 -0800

    switched back to masonry

--- ivys.js
+++ ivys.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
 
+    var $grid = $('.grid').imagesLoaded( function() {
+      // init Isotope after all images have loaded
+      $grid.masonry({
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+      });
+    });
+});
