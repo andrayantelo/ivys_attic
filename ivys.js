d1d0aaa2f0fa0c56e113802a50cc5bb140211dc9
andrayantelo <andrayantelo@gmail.com>
2017-03-07 10:53:44 -0800

    Changed css and html to use the new plugin;

--- ivys.js
+++ ivys.js
@@ -1,19 +1,1 @@
-$(document).ready(function() {
-
-    
-    var $grid = $('.grid').imagesLoaded( function() {
-      // init Isotope after all images have loaded
-      $grid.isotope({
-        // options
-        itemSelector: '.grid-item',
-        layoutMode: 'masonry',
-        percentPosition: true,
-        masonry: {
-            // use element for option
-            columnWidth: 100
-        }
-      });
-    });
-   
-});		 
 
