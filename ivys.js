cf35f5086da1a3761250ec8543828a91149efba8
andrayantelo <andrayantelo@gmail.com>
2017-03-20 12:32:12 -0700

    added a comment

--- ivys.js
+++ ivys.js
@@ -5,11 +5,11 @@ function IvyApp($grid) {
 };
 
 IvyApp.prototype.addItem = function(imageFileName) {
+    // adds an image to the masonry grid
     var $image =  $('<div class="grid-item"><a href="#"><div class="thumbnail">' +
     '<img src="images/' + imageFileName + '"class="img img-thumbnail"/></div></a>' +
     '</div>');
     
-
     this.$grid
       .append($image)
       .masonry( 'appended', $image)
