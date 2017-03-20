68ea60bcc2064bfda2a14493056aee50ffe57c4e
andrayantelo <andrayantelo@gmail.com>
2017-03-20 11:20:25 -0700

    moved masonry initialization to the html file

--- ivys.js
+++ ivys.js
@@ -1,12 +1,1 @@
-$(document).ready(function() {
 
-    var $grid = $('.grid').imagesLoaded( function() {
-      // init Isotope after all images have loaded
-      $grid.masonry({
-        // options
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-sizer',
-        percentPosition: true
-      });
-    });
-});
