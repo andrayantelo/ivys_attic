d1b4b94adba82095edc83187d11bb1ebf79077b2
andrayantelo <andrayantelo@gmail.com>
2017-03-20 12:12:57 -0700

    working on an addItem method for IvyApp;

--- ivys.js
+++ ivys.js
@@ -1,1 +1,27 @@
+function IvyApp($grid) {
+    this.$mainGrid = $('#mainGrid');
+    this.$grid = $grid;
+    
+};
+
+IvyApp.prototype.addItem = function(imageFileName) {
+    var image =  '<div class="grid-item"><a href="#"><div class="thumbnail">' +
+    '<img src="images/' + imageFileName + '"class="img img-thumbnail"/></div></a>' +
+    '</div>';
+    
+
+    this.$grid.masonry()
+      .append(image)
+      .masonry( 'appended', image)
+    
+};
+
+IvyApp.prototype.removeItem = function() {
+};
+
+IvyApp.prototype.addTag = function() {
+};
+
+IvyApp.prototype.removeTag = function() {
+};
 
